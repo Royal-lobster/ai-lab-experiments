2ff7f02567406f9ceccf7cb4cc8c0793389a9c63
Srujan Gurram <52039218+Royal-lobster@users.noreply.github.com>
2022-02-02 04:34:01 -0500

    Update 8-puzzle.py

--- 8-puzzle.py
+++ 8-puzzle.py
@@ -6,73 +6,52 @@ class Node:
         self.fval = fval
 
     def generate_child(self):
-        """ Generate child nodes from the given node by moving the blank space
-            either in the four directions {up,down,left,right} """
-        x,y = self.find(self.data,'_')
-        """ val_list contains position values for moving the blank space in either of
-            the 4 directions [up,down,left,right] respectively. """
-        val_list = [[x,y-1],[x,y+1],[x-1,y],[x+1,y]]
-        children = []
-        for i in val_list:
-            child = self.shuffle(self.data,x,y,i[0],i[1])
-            if child is not None:
-                child_node = Node(child,self.level+1,0)
-                children.append(child_node)
-        return children
+        # find the blank possition in the current state
+        blank_pos = None
+        for x in range(3):
+                for y in range(3):
+                    if self.data[x][y] == "_":
+                        blank_pos = [x, y]
+                        break
+        # find the possible moves
+        moves = []
+        x, y = blank_pos[0], blank_pos[1]
+        if x > 0: moves.append((x-1, y))
+        if x < 2: moves.append((x+1, y))
+        if y > 0: moves.append((x, y-1))
+        if y < 2: moves.append((x, y+1))
         
-    def shuffle(self,puz,x1,y1,x2,y2):
-        """ Move the blank space in the given direction and if the position value are out
-            of limits the return None """
-        if x2 >= 0 and x2 < len(self.data) and y2 >= 0 and y2 < len(self.data):
-            temp_puz = []
-            temp_puz = self.copy(puz)
-            temp = temp_puz[x2][y2]
-            temp_puz[x2][y2] = temp_puz[x1][y1]
-            temp_puz[x1][y1] = temp
-            return temp_puz
-        else:
-            return None
-            
+        children = []
+        for move in moves:
+            # generate child state for current move
+            new_state = [row[:] for row in self.data]        
+            new_state[blank_pos[0]][blank_pos[1]] = new_state[move[0]][move[1]]
+            new_state[move[0]][move[1]] = "_"   
 
-    def copy(self,root):
-        """ Copy function to create a similar matrix of the given node"""
-        temp = []
-        for i in root:
-            t = []
-            for j in i:
-                t.append(j)
-            temp.append(t)
-        return temp    
-            
-    def find(self,puz,x):
-        """ Specifically used to find the position of the blank space """
-        for i in range(0,len(self.data)):
-            for j in range(0,len(self.data)):
-                if puz[i][j] == x:
-                    return i,j
+            # append the new state to children
+            child_node = Node(new_state,self.level+1,0)
+            children.append(child_node)
 
+        return children
 
 class Puzzle:
     def __init__(self,size):
-        """ Initialize the puzzle size by the specified size,open and closed lists to empty """
         self.n = size
         self.open = []
         self.closed = []
 
-    def accept(self):
-        """ Accepts the puzzle from the user """
-        puz = []
-        for i in range(0,self.n):
-            temp = input().split(" ")
-            puz.append(temp)
-        return puz
+    def take_input(self, msg):
+        print(msg)
+        data = []
+        for x in range(3):
+            row = input().split(" ")
+            data.append(row)
+        return data
 
     def f(self,start,goal):
-        """ Heuristic Function to calculate hueristic value f(x) = h(x) + g(x) """
-        return self.h(start.data,goal)+start.level
+        return self.h(start.data,goal) + start.level
 
     def h(self,start,goal):
-        """ Calculates the different between the given puzzles """
         temp = 0
         for i in range(0,self.n):
             for j in range(0,self.n):
@@ -82,39 +61,48 @@ class Puzzle:
         
 
     def process(self):
-        """ Accept Start and Goal Puzzle state"""
-        print("Enter the start state matrix \n")
-        start = self.accept()
-        print("Enter the goal state matrix \n")        
-        goal = self.accept()
+        # accept the start and goal states as input
+        start = self.take_input("\nEnter start state: ")
+        goal = self.take_input("\nEnter goal state: ")
 
+        # make the start node
         start = Node(start,0,0)
         start.fval = self.f(start,goal)
-        """ Put the start node in the open list"""
+
+        # place the start node in open list
         self.open.append(start)
-        print("\n\n")
+        
+        # start traversing all states
+        print("\nSteps to solve : ")
         while True:
+            print("\n  ￬  \n")
+
+            # get the first item from open list
             cur = self.open[0]
-            print("")
-            print("  | ")
-            print("  | ")
-            print(" \\\'/ \n")
+
+            # print the current state
             for i in cur.data:
                 for j in i:
                     print(j,end=" ")
                 print("")
-            """ If the difference between current and goal node is 0 we have reached the goal node"""
+
+            # if the current state is goal state, break
             if(self.h(cur.data,goal) == 0):
                 break
-            for i in cur.generate_child():
-                i.fval = self.f(i,goal)
-                self.open.append(i)
+
+            # produce all child states and append to open list
+            for child in cur.generate_child():
+                child.fval = self.f(child,goal)
+                self.open.append(child)
+
+            # delete the current state in open list and add append it to closed list
             self.closed.append(cur)
             del self.open[0]
 
-            """ sort the opne list based on f value """
+            # sort the open list based on fval
             self.open.sort(key = lambda x:x.fval,reverse=False)
 
 
-puz = Puzzle(3)
-puz.process()
+if __name__ == "__main__":
+    puz = Puzzle(3)
+    puz.process()
